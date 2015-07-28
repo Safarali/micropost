ad70c98bff56711f780900cb3a4333d975c258ce
Safarali <mr.safarali@gmail.com>
2015-07-28 14:34:29 -0700

    Eliminate repetitions, setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+def setup
+  @base_title = "Welcome to TinyTwitter"
+end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Welcome to TinyTwitter"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Welcome to TinyTwitter"
+    assert_select "title", "Help | #{@base_title}"
   end
   
   test "should get about" do
   	get :about
   	assert_response :success
-    assert_select "title", "About | Welcome to TinyTwitter"
+    assert_select "title", "About | #{@base_title}"
+
   end
 end

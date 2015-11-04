116f1343937877f5dbf585eafc0460878fe315da
Safarali <mr.safarali@gmail.com>
2015-11-04 12:46:49 -0800

    AWS added for production

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAI7CUZI7VVDQA5RCA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['AKIAIX3XTY6UO53YUEPQ'],
+      :aws_secret_access_key => ENV['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']
     }
     config.fog_directory     =  ENV['hitwitter']
   end

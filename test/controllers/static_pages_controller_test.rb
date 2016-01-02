4e0c09785c2a34cb639957919b7de5ff26b54f2d
Safarali <mr.safarali@gmail.com>
2016-01-02 11:19:51 -0800

    Repushing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Welcome to hiTwitter"
+    assert_select "title", "Welcome to micropost"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Welcome to hiTwitter"
+    assert_select "title", "Help | Welcome to micropost"
   end
   
   test "should get about" do
   	get :about
   	assert_response :success
-    assert_select "title", "About | Welcome to hiTwitter"
+    assert_select "title", "About | Welcome to micropost"
   end
 
   test "should get contact" do
     get :about
     assert_response :success
-    assert_select "title", "About | Welcome to hiTwitter"
+    assert_select "title", "About | Welcome to micropost"
   end
 
 

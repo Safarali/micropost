49b66f02fe3cbbe7bcaf691f5f3be733058936f7
Safarali <mr.safarali@gmail.com>
2016-01-01 21:56:37 -0800

    Testing

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,12 +1,5 @@
 RailsAdmin.config do |config|
 
-
-config.authorize_with do
-    authenticate_or_request_with_http_basic('Login required') do |username, password|
-      username == Rails.application.secrets.user &&
-      password == Rails.application.secrets.password
-    end
-  end
   ### Popular gems integration
 
   ## == Devise ==
